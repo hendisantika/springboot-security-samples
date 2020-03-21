02cc386482022ca2d4e573af92702384cfb6de1e
Hendi Santika <hendisantika@gmail.com>
2020-03-21 13:38:27 +0700

    #9 - Add loginWithValidUserThenAuthenticated Test Case

--- src/test/java/com/hendisantika/springbootsecuritysamples/SpringbootSecuritySamplesApplicationTests.java
+++ src/test/java/com/hendisantika/springbootsecuritysamples/SpringbootSecuritySamplesApplicationTests.java
@@ -1,13 +1,30 @@
 package com.hendisantika.springbootsecuritysamples;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.FormLoginRequestBuilder;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SpringbootSecuritySamplesApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
+    public void loginWithValidUserThenAuthenticated() throws Exception {
+        FormLoginRequestBuilder login = formLogin()
+                .user("user")
+                .password("pass");
+
+        mockMvc.perform(login)
+                .andExpect(authenticated().withUsername("user"));
     }
 
 }
