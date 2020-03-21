058a017263221456c59387e2a666f125a42e26a1
Hendi Santika <hendisantika@gmail.com>
2020-03-21 13:44:18 +0700

    #14 - Add loginWithRoleUserThenExpectUserSpecificContent Test Case

--- src/test/java/com/hendisantika/springbootsecuritysamples/SpringbootSecuritySamplesApplicationTests.java
+++ src/test/java/com/hendisantika/springbootsecuritysamples/SpringbootSecuritySamplesApplicationTests.java
@@ -1,5 +1,7 @@
 package com.hendisantika.springbootsecuritysamples;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.Matcher;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -8,10 +10,12 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.FormLoginRequestBuilder;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -61,5 +65,29 @@ class SpringbootSecuritySamplesApplicationTests {
         mockMvc.perform(get("/index"))
                 .andExpect(status().isOk());
     }
+
+
+    @Test
+    @WithMockUser(roles = "USER")
+    public void loginWithRoleUserThenExpectUserSpecificContent() throws Exception {
+        mockMvc.perform(get("/index"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("This content is only shown to users.")))
+                .andExpect(content().string(doesNotContainString("This content is only shown to administrators.")));
+    }
+
+//    @Test
+//    @WithMockUser(roles = "ADMIN")
+//    public void loginWithRoleAdminThenExpectAdminSpecificContent() throws Exception {
+//        mockMvc.perform(get("/index"))
+//                .andExpect(status().isOk())
+//                .andExpect(content().string(containsString("This content is only shown to administrators.")))
+//                .andExpect(content().string(doesNotContainString("This content is only shown to users.")));
+//    }
+
+    private Matcher<String> doesNotContainString(String s) {
+        return CoreMatchers.not(containsString(s));
+    }
+
 
 }
