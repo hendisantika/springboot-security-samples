18d386e4b12cf13faa4f73aaa217f1c0ec49230f
Hendi Santika <hendisantika@gmail.com>
2020-03-21 14:02:30 +0700

    #19 - Add another Security Config

--- src/main/java/com/hendisantika/springbootsecuritysamples/config/WebSecurityConfig.java
+++ src/main/java/com/hendisantika/springbootsecuritysamples/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,15 +20,24 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private AuthenticationSuccessHandler authenticationSuccessHandler;
+
+    @Autowired
+    public WebSecurityConfig(AuthenticationSuccessHandler authenticationSuccessHandler) {
+        this.authenticationSuccessHandler = authenticationSuccessHandler;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                 .antMatchers("/css/**").permitAll()
+                .antMatchers("/admin").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login")
+                .successHandler(authenticationSuccessHandler)
                 .defaultSuccessUrl("/index")
                 .permitAll()
                 .and()
@@ -40,6 +50,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
-                .withUser("user").password("{noop}pass").roles("USER");
+                .withUser("user").password("{noop}pass").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}pass").roles("ADMIN");
+
     }
 }
