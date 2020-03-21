fe6e78247c421562495c4f48c58d3c2f81112647
Hendi Santika <hendisantika@gmail.com>
2020-03-21 13:12:17 +0700

    #2 - Add MvcConfig

---
+++ src/main/java/com/hendisantika/springbootsecuritysamples/config/MvcConfig.java
@@ -0,0 +1,25 @@
+package com.hendisantika.springbootsecuritysamples.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-security-samples
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/03/20
+ * Time: 13.12
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("login");
+        registry.addViewController("/index").setViewName("index");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
