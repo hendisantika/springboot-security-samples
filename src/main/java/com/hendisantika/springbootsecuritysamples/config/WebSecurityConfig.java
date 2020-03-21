8b5b57ebbbbe493be95e196749a5c4f2de0afad7
Hendi Santika <hendisantika@gmail.com>
2020-03-21 13:12:39 +0700

    #3 - Add WebSecurityConfig

---
+++ src/main/java/com/hendisantika/springbootsecuritysamples/config/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+package com.hendisantika.springbootsecuritysamples.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-security-samples
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/03/20
+ * Time: 13.12
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/css/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/index")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll()
+                .and().csrf().disable(); // we'll enable this in a later blog post
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("{noop}pass").roles("USER");
+    }
+}
