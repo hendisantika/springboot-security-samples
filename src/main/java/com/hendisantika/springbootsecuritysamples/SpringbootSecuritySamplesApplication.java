032d453af71f250b6ee5a0ef8e3056a0a5d7fab6
Hendi Santika <hendisantika@gmail.com>
2020-03-21 13:10:55 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootsecuritysamples/SpringbootSecuritySamplesApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootsecuritysamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecuritySamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSecuritySamplesApplication.class, args);
+    }
+
+}
